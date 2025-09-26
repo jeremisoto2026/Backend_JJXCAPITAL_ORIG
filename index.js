df03bceeb90715a2c7788845d840bb4c65b07bde
jeremisoto2026 <jeremisoto2022@gmail.com>
2025-09-26 19:33:48 +0200

    Actualizar index.js

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(rateLimit({ windowMs: 60 * 1000, max: 60 })); // 60 req/min
-
 app.use('/api', binanceRoutes);
 
 app.get('/', (req, res) => res.send('Backend JJXCAPITAL activo'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Backend listening on port ${PORT}`));
+// 👇 Exportamos app para que Vercel lo use
+module.exports = app;
